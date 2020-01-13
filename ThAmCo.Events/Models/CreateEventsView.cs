7decbac27a90c2a3096615736e840eaa88d19eac
TAHOMI RASIKH <S6105692@live.tees.ac.uk>
2020-01-13 13:36:31 +0000

    Added Two new View Models

--- ThAmCo.Events/Models/CreateEventsView.cs
+++ ThAmCo.Events/Models/CreateEventsView.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ThAmCo.Events.Data;
 
-namespace ThAmCo.Events.Data
+namespace ThAmCo.Events.Models
 {
-    public class Event
+    public class CreateEventsViewModel
     {
         public int Id { get; set; }
 
         [Required, Display(Name = "Event Name")]
         public string Title { get; set; }
 
+        [Required]
         public DateTime Date { get; set; }
 
         public TimeSpan? Duration { get; set; }
@@ -18,13 +22,10 @@ namespace ThAmCo.Events.Data
         [Required, Display(Name = "Event Type")]
         public string TypeId { get; set; }
 
-        [Display(Name = "Guest Bookings")]
-        public List<GuestBooking> Bookings { get; set; }
-
-        [Display(Name = "Staff Bookings")]
-        public List<StaffBooking> StaffBookings { get; set; }
-
-        [Display(Name = "Venue Name")]
+        [Required, Display(Name = "Venue Name")]
         public string VenueCode { get; set; }
+
+        [Required(ErrorMessage = "Make sure you have created at least one staff member before trying to create an event!"), Display(Name = "Staff Name")]
+        public int StaffId { get; set; }
     }
 }
---
+++ ThAmCo.Events/Models/EventsView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Models
+{
+    public class EventsView
+    {
+        public class EventsViewModel
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; }
+
+            public DateTime Date { get; set; }
+
+            public TimeSpan? Duration { get; set; }
+
+            public string TypeId { get; set; }
+
+            public List<GuestBooking> Booking { get; set; }
+
+            public List<StaffBooking> StaffBooking { get; set; }
+
+            public string TypeValue { get; set; }
+
+            public string VenueCode { get; set; }
+        }
+    }
+}
